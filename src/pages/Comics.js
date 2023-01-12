4a382c5d5dfe36d075f96413b7a7b54753de96a2
astorrena <astorrena@gmail.com>
2023-01-12 21:56:54 +0100

    Primeros pasos

---
+++ src/pages/Comics.js
@@ -0,0 +1,3 @@
+export default function Comics() {
+  return <div>Estamos en Comics</div>;
+}
---
+++ src/pages/Error404.js
@@ -0,0 +1,3 @@
+export default function Error404() {
+  return <div>Estamos en la página de Error 404</div>;
+}
